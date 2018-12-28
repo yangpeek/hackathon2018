37c6559d90157d77d520a364f05e90fe0222e11e
Qi Wang <wq86428@gmail.com>
2018-12-28 10:58:26 -0500

    fixed the final route

--- file_ops/filemerger/filemerger_test.js
+++ file_ops/filemerger/filemerger_test.js
@@ -1,8 +1,27 @@
 #!/usr/bin/env node
 
-var fs = require("fs");
-var fileMerger = require('./filemerger.js');
-var payload = fs.readFileSync("delta.json");
-var delta_obj = JSON.parse(payload);
-fileMerger.mergeJsonToFiles(delta_obj, "./", "base", "json");
+var delta_json = {
+    l1: {
+        l2: {
+            l31: 5
+        }
+    }
+};
 
+var full_json = {
+    l1: {
+        l2: {
+            l31: 1,
+            l32: "abc"
+        }
+    },
+    l11: {
+        l2: {
+            l3: false
+        }
+    }
+};
+
+var jsonMerger=require('./filemerger.js');
+var new_full = jsonMerger.mergeJson(delta_json, full_json);
+console.log(new_full);
